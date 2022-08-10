f903e03489e2832df384b8382a894d47de8a9858
j1xlte-gtelwifiue <110877891+j1xlte-gtelwifiue@users.noreply.github.com>
2022-08-10 16:02:45 -0600

    j1xlte: Add halium-boot initramfs download script

---
+++ update-initramfs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# This script will update the halium-boot initramfs to the latest version from GitHub.
+# This is needed because the automatic download doesn't work in Halium 10.
+#
+
+HALIUM_BOOT_DIR=halium/halium-boot/
+DEVICE_DIR=device/samsung/j1xlte/
+if [ ! -f "$HALIUM_BOOT_DIR/get-initrd.sh" ]; then
+	HALIUM_BOOT_DIR=../../../halium/halium-boot/
+	DEVICE_DIR=.
+
+	if [ ! -f "$HALIUM_BOOT_DIR/get-initrd.sh" ]; then
+		echo "This script must be run from the build root directory or device/samsung/j1xlte!"
+		exit 1
+	fi
+fi
+
+"$HALIUM_BOOT_DIR/get-initrd.sh" arm "$DEVICE_DIR/initramfs.gz"
+
+grep "BOARD_USE_LOCAL_INITRD := true" "$DEVICE_DIR/BoardConfig.mk" > /dev/null
+if [ ! $? -eq 0 ]; then
+	echo >> "$DEVICE_DIR/BoardConfig.mk"
+	echo "### Ubuntu Touch ###" >> "$DEVICE_DIR/BoardConfig.mk"
+	echo "BOARD_USE_LOCAL_INITRD := true" >> "$DEVICE_DIR/BoardConfig.mk"
+	echo "### End Ubuntu Touch ###" >> "$DEVICE_DIR/BoardConfig.mk"
+fi
